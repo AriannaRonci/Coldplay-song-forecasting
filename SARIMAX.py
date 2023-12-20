a7910e6b934a360601acfb01065e9bf20292eccb
AliceMoretti00 <morettialice@outlook.it>
2023-12-20 12:02:16 +0100

    sarimax

--- SARIMAX.py
+++ SARIMAX.py
@@ -5,15 +5,50 @@ import statsmodels.api as sm
 file = 'dataset/coldplay_grouped_by.csv'
 coldplay = pd.read_csv(file)
 
-# ACF, PACF graphs to help determine order of ARIMA model, again statsmodel has these handy functions built-in
-fig = plt.figure(figsize=(12,8))
-ax1 = fig.add_subplot(211)
-fig = sm.graphics.tsa.plot_acf(coldplay['Streams'][1:], lags=40, ax=ax1)   # first value of diff is NaN
-ax2 = fig.add_subplot(212)
-fig = sm.graphics.tsa.plot_pacf(coldplay['Streams'][1:], lags=40, ax=ax2)
-plt.show()
+coldplay['Date'] = pd.to_datetime(coldplay['Date'])
+coldplay.set_index('Date', inplace=True)
 
-file = 'dataset/coldplay.csv'
-coldplay = pd.read_csv(file)
-coldplay = coldplay.groupby(['Track Name'])['Streams'].sum().reset_index()
-print(coldplay.sort_values(by=['Streams']))
+streams = coldplay['Streams']
+
+# Construct the model
+mod = sm.tsa.SARIMAX(streams, order=(1, 0, 0), trend='c')
+# Estimate the parameters
+res = mod.fit(disp=0)
+
+print(res.summary())
+
+# The default is to get a one-step-ahead forecast
+forecast_result = res.forecast()
+print(f"Forecast Result: {forecast_result}\n")
+
+fcast_res1 = res.get_forecast()
+# Most results are collected in the `summary_frame` attribute.
+# Here we specify that we want a confidence level of 90%
+print("Summary Frame with 90% Confidence Level:")
+print(fcast_res1.summary_frame(alpha=0.1))
+
+fcast_res2 = res.forecast(steps=2)
+print(f"\nForecast Result for the Next 2 Steps: \n{fcast_res2}")
+
+# Plot result
+fig, ax = plt.subplots(figsize=(15, 5))
+
+# Plot the data
+streams.plot(ax=ax, label='Historical Data')
+
+# Construct the forecast
+forecast_steps = 56
+fcast = res.get_forecast(steps=forecast_steps).summary_frame()
+forecast_index = pd.date_range(start=streams.index[-1] + pd.DateOffset(1), periods=forecast_steps, freq='D')
+fcast.index = forecast_index
+
+fcast['mean'].plot(ax=ax, style='k--', label='Forecast')
+ax.fill_between(fcast.index, fcast['mean_ci_lower'], fcast['mean_ci_upper'], color='k', alpha=0.1)
+
+plt.xlabel('Date')
+plt.ylabel('Streams')
+plt.title('SARIMAX Forecast')
+
+plt.legend()
+plt.savefig('plots/sarimax_forecast.png', bbox_inches='tight')
+plt.show()
