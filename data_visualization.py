4b0dc3fc0a899bb9e43e0953fa4736874267148d
AriannaRonci <s1093926@studenti.univpm.it>
2023-12-20 13:13:41 +0100

    decomposizione

--- data_visualization.py
+++ data_visualization.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.dates import MonthLocator
+from statsmodels.tsa.seasonal import seasonal_decompose
 
 file_coldplay = 'dataset/coldplay_grouped_by.csv'
 coldplay = pd.read_csv(file_coldplay)
@@ -39,8 +40,52 @@ def streams_by_day(dataset, name):
     plt.show()
 
 
+def decompose(dataset, model):
+
+    dataset = dataset.iloc[:,1:3]
+    tdi = pd.DatetimeIndex(dataset.Date)
+    dataset.set_index(tdi, inplace=True)
+    dataset.drop(columns='Date', inplace=True)
+    dataset.index.name = 'datetimeindex'
+    time_series = pd.Series(dataset['Streams'], index=tdi)
+    result = seasonal_decompose(time_series, model=model, period=7, extrapolate_trend='freq')
+
+    # Visualizza i componenti decomposti
+    trend = result.trend
+    seasonal = result.seasonal
+    residual = result.resid
+
+    plt.figure(figsize=(8,6))
+
+    plt.subplot(411)
+    plt.title(f'{model.capitalize()} decomposition')
+    plt.plot(dataset, label='Original')
+    plt.legend(loc='upper left')
+
+    plt.subplot(412)
+    plt.plot(trend, label='Trend')
+    plt.legend(loc='upper left')
+
+    plt.subplot(413)
+    plt.plot(seasonal, label='Seasonality')
+    plt.legend(loc='upper left')
+
+    plt.subplot(414)
+    plt.plot(residual, label='Residuals')
+    plt.legend(loc='upper left')
+
+    plt.tight_layout()
+    plt.savefig(f"plots/decomposition_{model}", bbox_inches='tight')
+    plt.show()
+
+
+'''
 streams_by_day(song, 'song')
 streams_by_date(song, 'song')
 
 streams_by_day(coldplay, 'coldplay')
 streams_by_date(coldplay, 'coldplay')
+'''
+
+decompose(coldplay,'multiplicative')
+decompose(coldplay,'additive')
